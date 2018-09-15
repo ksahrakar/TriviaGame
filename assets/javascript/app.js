a9d87d439c1364472288f0e488730e7150a7cabb
Kamran Sahrakar <41097862+ksahrakar@users.noreply.github.com>
2018-09-15 07:38:59 -0700

    initial data in arrays

---
+++ assets/javascript/app.js
@@ -0,0 +1,36 @@
+var answerChoices = [
+    ["George Harrison","John Lennon","Ringo Starr","Paul McCartney"],
+    ["John F. Kennedy","Ronald Reagan","Martin Luther King","Bill Gates"],
+    ["Dalai Lama","Socrates","Albert Einstein","Walt Whitman"],
+    ["Mae West", "Marilyn Monroe", "Kathryn Hepburn", "Gina Lollobrigida"],
+    ["Hugh Heffner","Donald Trump", "Plato", "Emily Dickinson"],
+    ["Johann Wolfgang von Goethe","Pablo Picasso","Winston Churchill","Mick Jagger"],
+    ["Roy Orbison","Jim Morrison","Charles Schulz","Jimmi Hendrix"],
+    ["Isaac Asimov","Ray Bradbury","Jonathan Swift","Isaac Newton"],
+    ["Wayne Gretzky","Pele","LeBron James","Michael Jordan"],
+    ["Ghandi","Lao Tzu","Buddha","Dalai Lama"]
+];
+
+var quotes = [
+    "Life is what happens when you're busy making other plans",
+    "Those who dare to fail miserably can achieve greatly",
+    "If you want to live a happy life, tie it to a goal, not to people or things",
+    "You only live once; but if you do it right, once is enough",
+    "Love is a serious mental disease",
+    "Only put off until tomorrow what you are willing to die having left undone",
+    "A friend is someone who gives you total freedom to be yourself",
+    "May you live all the days of your life",
+    "You miss 100 percent of the shots you never take",
+    "The journey of a thousand miles begins with one step"
+]
+
+var card = {
+    quote: quotes,
+    answer: answerChoices,
+    correctA: [1,0,2,0,2,1,1,2,0,1],
+}
+
+// An accurate timer based on clock
+function timeClock(){
+    
+}
